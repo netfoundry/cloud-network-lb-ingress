020f633daaafe29b4317f0e1234c2fea796ba80d
dariuszSki <dariusz.sliwinski@netfoundry.io>
2024-07-26 22:46:24 -0400

    added a script to configure infra and run verification test against it

---
+++ AWS/tf-provider/test_cases.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+get_nf_network_id () {
+
+    export network_list=`curl --silent --location --request GET "https://gateway.production.netfoundry.io/core/v2/networks" \
+           --header "Content-Type: application/json" \
+           --header "Authorization: $token_type $token"`
+    export NETWORK_ID=`echo $network_list | jq -r --arg NF_NETWORK_NAME "$NF_NETWORK_NAME" '._embedded.networkList[] | select(.name==$NF_NETWORK_NAME).id'`
+
+}
+
+get_nf_er_reg_keys () {
+
+    get_nf_network_id
+    export ER_RESP=`curl --silent --location --request POST "https://gateway.production.netfoundry.io/core/v2/edge-routers" \
+        --header "Content-Type: application/json" \
+        --header "Authorization: $token_type $token" \
+        --data "{
+            \"name\":\"$ER\",
+            \"networkId\":\"$NETWORK_ID\",
+            \"linkListener\":false,
+            \"attributes\":[],
+            \"tunnelerEnabled\": true,
+            \"noTraversal\": false
+        }"`
+    export ER_ID=`jq -r .id <<< "$ER_RESP"`
+    sleep 10
+    export ER_KEY_JSON=`curl --silent --location --request POST "https://gateway.production.netfoundry.io/core/v2/edge-routers/$ER_ID/registration-key" \
+        --header "Content-Type: application/json" \
+        --header "Authorization: $token_type $token"`
+    export ER_KEY=`jq -r .registrationKey <<< "$ER_KEY_JSON"` 
+    jq ".er_map_be[$COUNT].edgeRouterKey = \"$ER_KEY\"" input_vars.tfvars.json > "tmp" && mv "tmp" input_vars.tfvars.json
+    ER_IDENT_RESP=`curl --silent --location --request GET "https://gateway.production.netfoundry.io/core/v2/endpoints?name=$ER" \
+        --header "Content-Type: application/json" \
+        --header "Authorization: $token_type $token"`
+    ER_IDENT_ID=`jq -r ._embedded.endpointList[0].id <<< "$ER_IDENT_RESP"`
+    curl --silent --location --request PATCH "https://gateway.production.netfoundry.io/core/v2/endpoints/$ER_IDENT_ID" \
+        --header "Content-Type: application/json" \
+        --header "Authorization: $token_type $token" \
+        --data "{\"attributes\": [\"#$ATTRIBUTE\"]}"
+
+}
+
+delete_nf_er () {
+
+    export ER_RESP=`curl --silent --location --request GET "https://gateway.production.netfoundry.io/core/v2/edge-routers?name=$ER" \
+        --header "Content-Type: application/json" \
+        --header "Authorization: $token_type $token"`
+    export ER_ID=`jq -r ._embedded.edgeRouterList[0].id <<< "$ER_RESP"`
+    curl --silent --location --request DELETE "https://gateway.production.netfoundry.io/core/v2/edge-routers/$ER_ID" \
+        --header "Content-Type: application/json" \
+        --header "Authorization: $token_type $token"
+
+}
+
+get_nf_token () {
+
+    export NF_API_CLIENT_ID=`jq -r .clientId $NF_API_CREDENTIALS_PATH`
+    export NF_API_CLIENT_SECRET=`jq -r .password $NF_API_CREDENTIALS_PATH`
+    export RESPONSE=`curl --silent --location --request POST "https://netfoundry-production-xfjiye.auth.us-east-1.amazoncognito.com/oauth2/token" \
+                        --header "Content-Type: application/x-www-form-urlencoded" \
+                        --user "$NF_API_CLIENT_ID:$NF_API_CLIENT_SECRET" --data-urlencode "grant_type=client_credentials"`
+    export token=`echo $RESPONSE |jq -r .access_token`
+    export token_type=`echo $RESPONSE |jq -r .token_type`
+
+}
+
+run () {
+
+    get_nf_token
+    
+    terraform init
+    
+    terraform workspace new us-west-2
+    export ATTRIBUTE="bind-services"
+    # router1
+    export ER=$REGION2_ER1
+    export COUNT=0
+    get_nf_er_reg_keys
+    # router2
+    export ER=$REGION2_ER2
+    export COUNT=1
+    get_nf_er_reg_keys
+    terraform apply -var-file input_vars.tfvars.json -var 'region=us-west-2' -auto-approve
+
+    terraform workspace new us-east-2
+    export ATTRIBUTE="novis"
+    # router1
+    export ER=$REGION1_ER1
+    export COUNT=0
+    get_nf_er_reg_keys
+    # router2
+    export ER=$REGION1_ER2
+    export COUNT=1
+    get_nf_er_reg_keys
+    terraform apply -var-file input_vars.tfvars.json -var 'region=us-east-2' -auto-approve
+    
+}
+
+env_vars () {
+
+    export NF_API_CREDENTIALS_PATH="$HOME/.netfoundry/credentials.json"
+    export NF_NETWORK_NAME="dariuszdev"
+    export REGION1_ER1="nf-be-er01-useast2"
+    export REGION1_ER2="nf-be-er02-useast2"
+    export REGION2_ER1="nf-be-er01-uswest2"
+    export REGION2_ER2="nf-be-er02-uswest2"
+    
+}
+
+cleanup () {
+
+    declare -a ER_LIST=("$REGION1_ER1" "$REGION1_ER2" "$REGION2_ER1" "$REGION2_ER2")
+    get_nf_token
+    for ER in "${ER_LIST[@]}"
+    do 
+        delete_nf_er
+    done
+
+    terraform workspace select us-east-2
+    terraform apply -destroy -var-file input_vars.tfvars.json -var 'region=us-east-2' -auto-approve
+    terraform workspace select us-west-2
+    terraform apply -destroy -var-file input_vars.tfvars.json -var 'region=us-west-2' -auto-approve
+    terraform workspace select default
+    terraform workspace delete us-east-2
+    terraform workspace delete us-west-2
+}
+
+# Main Program
+if [ $# -ne 1 ]; then
+     echo "You need to provide an action [run/cleanup]"
+     exit 0
+fi
+ 
+env_vars
+
+case $1 in
+
+    run)
+        run
+        ;;
+
+    cleanup)
+        cleanup
+        ;;
+    
+    *)
+    echo "unknown action"
+    exit 0
+    ;;
+
+esac
+
+
