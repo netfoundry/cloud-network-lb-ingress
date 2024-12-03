d0a6dee7cc9b5ed531db95368e2cf6b3f35ef384
dariuszSki <dariusz.sliwinski@netfoundry.io>
2024-12-02 23:07:49 -0500

    updated artifact download search

--- AWS/apps/dl_artifacts_zfw.sh
+++ AWS/apps/dl_artifacts_zfw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /usr/bin/gh auth login --with-token <<<$1
-asset=$(/usr/bin/gh api /repos/netfoundry/zfw/actions/artifacts --jq ".artifacts[$2] .archive_download_url")
+asset=$(/usr/bin/gh api /repos/netfoundry/zfw/actions/artifacts --jq '( last ((.artifacts | sort_by(.created_at)).[] | select(.name | endswith("router-amd64-deb")).archive_download_url))')
 /usr/bin/curl -Ls  "$asset"    -H "Accept: application/vnd.github.v3+json"    -H "Authorization: Bearer $1"  --output /opt/netfoundry/artifact.zip
 /usr/bin/unzip  -d /opt/netfoundry/ /opt/netfoundry/artifact.zip
 /usr/bin/dpkg -i /opt/netfoundry/zfw-router_*_amd64.deb
